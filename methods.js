07d009ad7bd8ece1e8a290016f0a96db4737e57b
Syed Jawad <jawadhussaini1804@gmail.com>
2024-05-21 13:13:42 +0500

    methods

---
+++ methods.js
@@ -0,0 +1,37 @@
+let students = "ali, aliya, ahmad,";
+let concat = students.concat(" fatima");
+console.log(concat);
+
+let student = "akhtar"
+let start = student.startsWith("a");
+console.log(start);
+
+let children = "hassan, aliya, farhan, bismil, furman, roqia, "
+let searching = children.search("i");
+console.log(searching);
+
+let nam = "jaWaD";
+let toLowerCase = nam.toLocaleLowerCase()
+let toUpperCase = nam.toUpperCase()
+console.log(toUpperCase)
+console.log(toLowerCase)
+
+let cutOut  = "A Software should have a consistency";
+let slice = cutOut.slice(1,25);
+console.log(slice)
+
+let myName = "jawad"
+let split = myName.split("")
+console.log(split)
+
+let under = "apple"
+let include = under.includes("l");
+console.log(include)
+
+let index = "fatim"
+let indexof = index.indexOf("i")
+console.log(indexof);
+
+let changing = "myname"
+let replace = changing.replace("name", "school");
+console.log(replace)
