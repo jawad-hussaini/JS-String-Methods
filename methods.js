60fe3a950612c15ffcda304dd077050c61c33c57
Syed Jawad <jawadhussaini1804@gmail.com>
2024-05-22 09:37:44 +0500

    all String Methods

--- methods.js
+++ methods.js
@@ -35,3 +35,55 @@ console.log(indexof);
 let changing = "myname"
 let replace = changing.replace("name", "school");
 console.log(replace)
+
+let replacing = "friendsname"
+let replaceAll = replacing.replaceAll("friendsname", "Kamal Hussain");
+console.log(replaceAll);
+
+let Strimer = "                Malik"
+let trimStart = Strimer.trimStart();
+console.log(trimStart);
+
+let Etrimer = "Hussain              "
+let trimEnd = Etrimer.trimEnd();
+console.log(trimEnd);
+
+let trimer = "               Coding               "
+let trim = trimer.trim();
+console.log(trim);
+
+let repeating = "Continue";
+let repeat = repeating.repeat(3);
+console.log(repeat);
+
+let Spad = "Coach";
+let padStart= Spad.padStart(6, "@");
+console.log(padStart);
+
+let Epad = "Learning";
+let padEnd = Epad.padEnd(9, "$");
+console.log(padEnd);
+
+let subG = "Hello";
+let substring = subG.substring(1,4)
+console.log(substring)
+
+let str = "Apple, Banana, Mango";
+let substr = str.substring(2,18);
+console.log(substr)
+
+let char = "Hello";
+let charAt = char.charAt(0);
+console.log(charAt);
+
+let Code = "Coding";
+let charCodeAt = Code.charCodeAt(2);
+console.log(charCodeAt);
+
+let At = "Hello World";
+let at = At.at(6);
+console.log(at);
+
+let sting = "Mikle";
+let string = sting[2];
+console.log(string);
